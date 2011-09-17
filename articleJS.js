48efe3cee489bb4561d15fd80ab280d2c4d4ea87
newtux <newtux@gmail.com>
2011-09-17 02:50:13 -0400

    Slight tweak

--- articleJS.js
+++ articleJS.js
@@ -15,7 +15,7 @@ function readCookie(name) {
 		var c=ca[i];
 		while(c.charAt(0)==' ')
 		c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ)==0)
+		if (c.indexOf(nameEQ)===0)
 			return c.substring(nameEQ.length,c.length);
 	}
 	return null;
